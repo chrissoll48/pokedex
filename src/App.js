89ea7368f6b99cea3845b8bd17dd964eff013b3d
chrissoll48 <chrissysoll@gmail.com>
2020-05-19 20:54:45 -0400

    Hello Pikachu first change
    Added text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Pikachu! Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Smile you are in qteen </p>
         <a
           className="App-link"
           href="https://reactjs.org"
