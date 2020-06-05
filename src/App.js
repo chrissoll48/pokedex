6df7c0764688d1009c3ad13506d7d46dfac8e101
chrissoll48 <chrissysoll@gmail.com>
2020-06-05 14:30:14 -0400

    Added Pokedex Header - Wk3
    Added a Header that displays the name of the Pokedex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import PokedexHeader from './components/PokedexHeader';
 import './App.css';
-import { Button } from 'semantic-ui-react'; 
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello Pikachu! Edit <code>src/App.js</code> and save to reload.
-        </h1>
-        <p> Smile you are in qteen </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <PokedexHeader author= {'@chrissoll48'} name={"Xtina"} /> 
     </div>
   );
 }
